bf6e2cd5e2a8bd70896a7e781a3edad23962b53f
Chase <ChaseEWhitney@gmail.com>
2020-01-17 13:46:38 -0600

    auth middleware unit testing

---
+++ tests/unit/middleware/auth.test.js
@@ -0,0 +1,23 @@
+// Auth middleware - Unit testing
+
+const jwt = require("jsonwebtoken");
+const jwtPrivateKey = require("../../../config/keys").jwtPrivateKey;
+const mongoose = require("mongoose");
+const auth = require("../../../middleware/auth");
+
+describe("auth middleware", () => {
+  it("should populate req.user if provided token is valid", () => {
+    const userObject = { _id: "123", name: "345" };
+    const token = jwt.sign(userObject, jwtPrivateKey);
+    const res = "";
+    const req = {
+      header: function() {
+        return token;
+      }
+    };
+    const next = () => {};
+    auth(req, res, next);
+
+    expect(req.user).toMatchObject(userObject);
+  });
+});
